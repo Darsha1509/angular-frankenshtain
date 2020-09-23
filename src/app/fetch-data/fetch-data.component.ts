af2763166e5020d2d224a1daf1e4e0db33686216
darya <kokorina.darya1509@gmail.com>
2020-09-23 21:43:11 +0300

    task2

--- src/app/fetch-data/fetch-data.component.ts
+++ src/app/fetch-data/fetch-data.component.ts
@@ -1,28 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html',
-  styleUrls: ['./fetch-data.component.css']
+  styleUrls: ['./fetch-data.component.css'],
 })
 export class FetchDataComponent implements OnInit {
-  inputValue: FormControl;
+  input: FormControl;
+  heroes: { name: string; birth_year: string; gender: string }[];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.inputValue = new FormControl();
+    this.input = new FormControl();
 
-    this.fetchData();
+    this.getInitialData();
   }
 
-  private fetchData() {
-    this.http.get('https://swapi.dev/api/people')
-    .subscribe(data => {
-      console.log(data);
+  private fetchData(url: string) {
+    return this.http.get(url).pipe(
+      map(
+        (data: {
+          results: { name: string; birth_year: string; gender: string }[];
+        }) => {
+          const peopleResult = data.results.map((person) => {
+            return {
+              name: person.name,
+              birth_year: person.birth_year,
+              gender: person.gender,
+            };
+          });
+          return peopleResult;
+        }
+      )
+    );
+  }
+
+  searchPerson() {
+    this.fetchData(
+      `https://swapi.dev/api/people/?search=${this.input.value}`
+    ).subscribe((data) => {
+      this.heroes = data;
     });
   }
 
+  getInitialData() {
+    this.fetchData('https://swapi.dev/api/people').subscribe((data) => {
+      this.heroes = data;
+    });
+  }
 }
